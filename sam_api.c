0575f857475754dadce7d8767c483b8155a40506
Tiernan Messmer <tiernan.messmer@gmail.com>
2025-08-18 22:27:03 +1000

    fix ATS handling for 14443a-4 cards
    preivously the ATS was always filled with the ATQA which caused
    some oddities with DESFire EV2 and newer cards
    
    this change populates the ATS which removes the need for the EV2
    workaround
    
    for Seos cards however the ATS does not appear to be being returned
    by the flipper firmware (perhaps an upstream bug?) but for Seos the
    ATS doesn't matter as long as "something" is present, so we retain
    the previous behaviour of filling in the ATQA for part4 cards

--- sam_api.c
+++ sam_api.c
@@ -21,10 +21,6 @@ char asn1_log[SEADER_UART_RX_BUF_SIZE] = {0};
 
 uint8_t updateBlock2[] = {RFAL_PICOPASS_CMD_UPDATE, 0x02};
 
-uint8_t ev2_request[] =
-    {0x00, 0xa4, 0x04, 0x00, 0x0a, 0xa0, 0x00, 0x00, 0x04, 0x40, 0x00, 0x01, 0x01, 0x00, 0x01, 0x00};
-uint8_t FILE_NOT_FOUND[] = {0x6a, 0x82};
-
 void* calloc(size_t count, size_t size) {
     return malloc(count * size);
 }
@@ -748,20 +744,14 @@ void seader_iso14443a_transmit(
     BitBuffer* rx_buffer = bit_buffer_alloc(SEADER_POLLER_MAX_BUFFER_SIZE);
 
     do {
-        if(credential->isDesfire && memcmp(buffer, ev2_request, len) == 0) {
-            FURI_LOG_I(TAG, "Intercept Desfire EV2 response and return File Not Found");
-            bit_buffer_append_bytes(rx_buffer, FILE_NOT_FOUND, sizeof(FILE_NOT_FOUND));
+        bit_buffer_append_bytes(tx_buffer, buffer, len);
 
-        } else {
-            bit_buffer_append_bytes(tx_buffer, buffer, len);
-
-            Iso14443_4aError error =
-                iso14443_4a_poller_send_block(iso14443_4a_poller, tx_buffer, rx_buffer);
-            if(error != Iso14443_4aErrorNone) {
-                FURI_LOG_W(TAG, "iso14443_4a_poller_send_block error %d", error);
-                seader_worker->stage = SeaderPollerEventTypeFail;
-                break;
-            }
+        Iso14443_4aError error =
+            iso14443_4a_poller_send_block(iso14443_4a_poller, tx_buffer, rx_buffer);
+        if(error != Iso14443_4aErrorNone) {
+            FURI_LOG_W(TAG, "iso14443_4a_poller_send_block error %d", error);
+            seader_worker->stage = SeaderPollerEventTypeFail;
+            break;
         }
 
         seader_capture_sio(tx_buffer, rx_buffer, credential);
@@ -1111,9 +1101,6 @@ NfcCommand seader_worker_card_detect(
     uint8_t uid_len,
     uint8_t* ats,
     uint8_t ats_len) {
-    UNUSED(ats);
-    UNUSED(ats_len);
-
     SeaderCredential* credential = seader->credential;
 
     CardDetails_t* cardDetails = 0;
@@ -1122,7 +1109,19 @@ NfcCommand seader_worker_card_detect(
 
     OCTET_STRING_fromBuf(&cardDetails->csn, (const char*)uid, uid_len);
     OCTET_STRING_t sak_string = {.buf = &sak, .size = 1};
-    OCTET_STRING_t atqa_string = {.buf = atqa, .size = 2};
+    OCTET_STRING_t ats_string = {0};
+    if(ats != NULL) {
+        ats_string.buf = ats;
+        ats_string.size = ats_len;
+    } else {
+        // technically I think this field is only ever meant to be ATS not ATQA
+        // for 14443a-4 cards but in practice, Seos cards seem to fail to read the
+        // ATS but the ATS doesn't matter for Seos as long as it's there otherwise
+        // the SAM thinks it's a part3 card, and filling it with the ATQA is what
+        // we did previously and seemed to work so eh
+        ats_string.buf = atqa;
+        ats_string.size = 2;
+    }
     uint8_t protocol_bytes[] = {0x00, 0x00};
 
     if(sak != 0 && atqa != NULL) { // type 4
@@ -1130,7 +1129,7 @@ NfcCommand seader_worker_card_detect(
         OCTET_STRING_fromBuf(
             &cardDetails->protocol, (const char*)protocol_bytes, sizeof(protocol_bytes));
         cardDetails->sak = &sak_string;
-        cardDetails->atqa = &atqa_string;
+        cardDetails->atqa = &ats_string;
         credential->isDesfire = seader_mf_df_check_card_type(atqa[0], atqa[1], sak);
         if(credential->isDesfire) {
             memcpy(credential->diversifier, uid, uid_len);
--- seader_worker.c
+++ seader_worker.c
@@ -281,12 +281,26 @@ NfcCommand seader_worker_poller_callback_iso14443_4a(NfcGenericEvent event, void
             size_t uid_len;
             const uint8_t* uid = nfc_device_get_uid(seader->nfc_device, &uid_len);
 
-            const Iso14443_3aData* iso14443_3a_data =
-                nfc_device_get_data(seader->nfc_device, NfcProtocolIso14443_3a);
+            const Iso14443_4aData* iso14443_4a_data =
+                nfc_device_get_data(seader->nfc_device, NfcProtocolIso14443_4a);
+            const Iso14443_3aData* iso14443_3a_data = iso14443_4a_get_base_data(iso14443_4a_data);
+            uint32_t ats_len = 0;
+            uint8_t* ats = NULL;
+            if(iso14443_4a_data->ats_data.t1_tk != NULL) {
+                // for some reason Seos cards fail to read the ATS
+                SimpleArray* ats_array = iso14443_4a_data->ats_data.t1_tk;
+                ats_len = simple_array_get_count(ats_array);
+                if(ats_len > 0xFF) {
+                    ats_len = 0;
+                }
+                if(ats_len > 0) {
+                    ats = simple_array_get_data(ats_array);
+                }
+            }
             uint8_t sak = iso14443_3a_get_sak(iso14443_3a_data);
 
             seader_worker_card_detect(
-                seader, sak, (uint8_t*)iso14443_3a_data->atqa, uid, uid_len, NULL, 0);
+                seader, sak, (uint8_t*)iso14443_3a_data->atqa, uid, uid_len, ats, ats_len);
 
             // nfc_set_fdt_poll_fc(event.instance, SEADER_POLLER_MAX_FWT);
             furi_thread_set_current_priority(FuriThreadPriorityLowest);
