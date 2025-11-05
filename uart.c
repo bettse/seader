0c5c411a48b01ed93f85fef912c02cbea8312290
Eric Betts <bettse@fastmail.fm>
2025-11-04 21:24:40 -0800

    asserts to try to catch crash

--- uart.c
+++ uart.c
@@ -269,8 +269,13 @@ SeaderUartBridge* seader_uart_enable(SeaderUartConfig* cfg, Seader* seader) {
 }
 
 void seader_uart_send(SeaderUartBridge* seader_uart, uint8_t* data, size_t len) {
+    furi_assert(seader_uart);
+    furi_assert(data);
     memcpy(seader_uart->tx_buf, data, len);
     seader_uart->tx_len = len;
+
+    // I've been getting a "[CRASH][ISR MemMgmt] NULL pointer dereference" here and the delay seems to fix it.
+    //furi_delay_ms(5);
     furi_thread_flags_set(furi_thread_get_id(seader_uart->tx_thread), WorkerEvtSamRx);
 }
 
