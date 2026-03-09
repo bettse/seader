079b2a7cc790ad95e212a0f298a53df61481e47f
CinderSocket <cindersocket@protonmail.ch>
2026-03-09 10:23:11 -0700

    Add zero-copy SNMP parser and host tests

--- lib/host_tests/test_main.c
+++ lib/host_tests/test_main.c
@@ -4,6 +4,7 @@ extern MunitSuite test_lrc_suite;
 extern MunitSuite test_ccid_logic_suite;
 extern MunitSuite test_t1_existing_suite;
 extern MunitSuite test_t1_protocol_suite;
+extern MunitSuite test_snmp_suite;
 
 int main(int argc, char* argv[]) {
     MunitSuite child_suites[] = {
@@ -11,6 +12,7 @@ int main(int argc, char* argv[]) {
         {"/t1", test_t1_existing_suite.tests, NULL, 1, MUNIT_SUITE_OPTION_NONE},
         {"/t1", test_t1_protocol_suite.tests, NULL, 1, MUNIT_SUITE_OPTION_NONE},
         {"/ccid", test_ccid_logic_suite.tests, NULL, 1, MUNIT_SUITE_OPTION_NONE},
+        {"/snmp", test_snmp_suite.tests, NULL, 1, MUNIT_SUITE_OPTION_NONE},
         {NULL, NULL, NULL, 0, 0},
     };
     MunitSuite main_suite = {
